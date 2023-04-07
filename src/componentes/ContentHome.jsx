5652b8b55680100498ef69206a2780a61dccc411
jacksonsouzasantos <jacksonsantos.tec@gmail.com>
2023-04-07 10:25:41 -0300

    feat: refatorando código video de backgound na pagina home

--- src/componentes/ContentHome.jsx
+++ src/componentes/ContentHome.jsx
@@ -1,21 +1,3 @@
-// import React from 'react';
-// import TypeWrite from './typeWrite';
-// import '../style/Home.css';
-// import BackgroundVideo from './BackgoundVideo';
-
-// export default function ContentHome() {
-//   return (
-//     <div className="home">
-//       <BackgroundVideo src="src/assets/background.mp4" />
-//       <div className="content">
-//         <div className="text">
-//           <TypeWrite className="write" texto="Bem-vindo ao meu portfólio" />
-//           <p>Eu sou um desenvolvedor de aplicativos web apaixonado por criar soluções criativas para problemas complexos.</p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
 import React from 'react';
 import TypeWrite from './typeWrite';
 import '../style/Home.css';
@@ -26,7 +8,7 @@ export default function ContentHome() {
       <div className="video-background">
         <video autoPlay muted loop className="video">
           <source src="src/assets/background.mp4" type="video/mp4" />
-          {/* Adicione outros formatos de vídeo aqui para compatibilidade com outros navegadores */}
+          <source src="src/assets/background.webm" type="video/webm"></source>
         </video>
       </div>
       <div className="content">
