52a5c240dbf9d911cb3ab729de0f8460f5edfa90
jacksonsouzasantos <jacksonsantos.tec@gmail.com>
2023-04-11 10:04:23 -0300

    [jackson santos] feat: Criando página About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import NavBar from '../componentes/NavBar';
 import '../style/About.css';
+import Footer from '../componentes/Footer';
+import mypicture from '../assets/mypicture.jpg';
+
 
 export default function About() {
-  return (
+return (
+  
     <>
-    <h1>About</h1>
-    <NavBar />
-    </>
-  )
+    <NavBar /><Footer /><div className="about-container">
+      <img src={mypicture} alt="Minha Foto" className="about-img" />
+      <div className="about-text">
+        <h2>Sobre Mim</h2>
+        <p>Meu nome é [seu nome], sou [descrição breve sobre você, por exemplo: estudante de programação, apaixonado por tecnologia].</p>
+        <p>Eu adoro aprender novas coisas e estou sempre em busca de desafios.</p>
+      </div>
+    </div></>
+);
 }
