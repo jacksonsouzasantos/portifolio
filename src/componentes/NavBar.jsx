e775e8335e45b05ba75ec8bf475c2748347990fd
jacksonsouzasantos <jacksonsantos.tec@gmail.com>
2023-05-18 10:19:14 -0300

    [jackson santos] feat: refatorando e melhorando a performace

--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -21,34 +21,26 @@ const NavBar = () => {
           {showMenu ? <FaTimes /> : <FaBars className="hamburger-icon" />}
         </div>
         <ul className={`nav-list ${showMenu ? 'show-menu' : ''}`} data-testid='nav-menu' role="menu">
-          <ul>
-            <li className="nav-item">
-              <NavLink to="/" onClick={hideMenu} aria-current="page" role="menuitem">
-                Home
-              </NavLink>
-            </li>
-          </ul>
-          <ul>
-            <li className="nav-item">
-              <NavLink exact="true" to="/about" onClick={hideMenu} role="menuitem">
-                About
-              </NavLink>
-            </li>
-          </ul>
-          <ul>
-            <li className="nav-item">
-              <NavLink exact="true" to="/projects" onClick={hideMenu} role="menuitem">
-                Projects
-              </NavLink>
-            </li>
-          </ul>
-          <ul>
-            <li className="nav-item">
-              <NavLink exact="true" to="/skills" onClick={hideMenu} role="menuitem">
-                Skills
-              </NavLink>
-            </li>
-          </ul>
+          <li className="nav-item">
+            <NavLink to="/" onClick={hideMenu} aria-current="page" role="menuitem">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/about" onClick={hideMenu} role="menuitem">
+              About
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/projects" onClick={hideMenu} role="menuitem">
+              Projects
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/skills" onClick={hideMenu} role="menuitem">
+              Skills
+            </NavLink>
+          </li>
         </ul>
       </nav>
     </div>
