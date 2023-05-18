7b544f49bacef27de0527a3ffd962c351226f92c
jacksonsouzasantos <jacksonsantos.tec@gmail.com>
2023-05-18 10:05:56 -0300

    [jackson santos] feat: refatorando e melhorando a performace

--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -15,36 +15,33 @@ const NavBar = () => {
   };
 
   return (
-    // <div className="nav-menu-container">
     <div className="nav-menu-container" role="navigation">
       <nav className={`nav-menu ${showMenu ? 'menu-open' : ''}`} role="menubar">
-        {/* <div className="hamburger-menu" aria-label="Abrir 
-        menu" onClick={handleMenu}>
-          {showMenu ? <FaTimes /> : <FaBars />}
-        </div> */}
- <div className={`hamburger-menu ${showMenu ? 'menu-open' : ''}`} aria-label="Abrir menu" onClick={handleMenu}>
-  {showMenu ? <FaTimes /> : <FaBars className="hamburger-icon"/>}
-</div>
+        <div className={`hamburger-menu ${showMenu ? 'menu-open' : ''}`} aria-label="Abrir menu" onClick={handleMenu}>
+          {showMenu ? <FaTimes /> : <FaBars className="hamburger-icon" />}
+        </div>
 
         <ul className={`nav-list ${showMenu ? 'show-menu' : ''}`} data-testid='nav-menu' role="menu">
           <li className="nav-item">
-          <NavLink to="/" onClick={hideMenu} aria-current="page">
-            {/* <NavLink to="/" onClick={hideMenu}> */}
+            <NavLink to="/" onClick={hideMenu} aria-current="page" role="menuitem">
               Home
             </NavLink>
           </li>
+
           <li className="nav-item">
-            <NavLink exact="true" to="/about" onClick={hideMenu}>
+            <NavLink exact="true" to="/about" onClick={hideMenu} role="menuitem">
               About
             </NavLink>
           </li>
+
           <li className="nav-item">
-            <NavLink exact="true" to="/projects" onClick={hideMenu}>
+            <NavLink exact="true" to="/projects" onClick={hideMenu} role="menuitem">
               Projects
             </NavLink>
           </li>
+
           <li className="nav-item">
-            <NavLink exact="true" to="/skills" onClick={hideMenu}>
+            <NavLink exact="true" to="/skills" onClick={hideMenu} role="menuitem">
               Skills
             </NavLink>
           </li>
